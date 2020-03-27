5c86fa8ecb15e996eb62d68f59f08c63d335759f
Adrien DUPUIS <adrien.dupuis@ez.no>
2020-03-27 15:26:22 +0100

    Add Varnish, Apache w/ PHP-FPM, Redis MariaDB & Solr containers and a builder.

---
+++ docker-compose.bash
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Stop on error
+#set -e;
+
+# eZ Platform logs removal
+rm -f var/logs/*.log;
+
+# Docker Containers Cluster Build (except Solr which needs vendor/ezsystems/ezplatform-solr-search-engine/)
+docker-compose up --build --detach varnish apache redis mariadb;
+
+# MariaDB: Server Wait & Version Fetch
+GET_MARIADB_VERSION_CMD="docker-compose exec mariadb mysql -proot -BNe 'SELECT VERSION();' | cut -d '-' -f 1 | head -n 1;";
+MARIADB_VERSION=`eval $GET_MARIADB_VERSION_CMD`;
+while [ -n "`echo $MARIADB_VERSION | grep 'ERROR';`" ]; do
+  echo 'Waiting for server inside mariadb container...';
+  sleep 3;
+  MARIADB_VERSION=`eval $GET_MARIADB_VERSION_CMD`;
+done;
+echo "MariaDB version: $MARIADB_VERSION";
+
+# Apache: Symfony parameters.yml
+cp docker/apache/parameters.yml app/config/parameters.yml;
+sed -i '' -e "s/MARIADB_VERSION/$MARIADB_VERSION/" app/config/parameters.yml;
+
+# Apache: Composer Authentication
+if [ ! -f auth.json ]; then
+  echo -n "eZ Platform Enterprise Edition Installation Key: "; read INSTALLATION_KEY;
+  echo -n "eZ Platform Enterprise Edition Token Password: "; read TOKEN_PASSWORD;
+  docker-compose exec --user www-data apache composer config http-basic.updates.ez.no ${INSTALLATION_KEY} ${TOKEN_PASSWORD};
+fi;
+
+# Apache: Composer Scripts' Timeout
+docker-compose exec --user www-data apache composer config --global process-timeout 0;
+
+# Apache: Composer Install
+docker-compose exec --user www-data apache composer install --no-interaction;
+
+# Solr: Docker Container Build (needs vendor/ezsystems/ezplatform-solr-search-engine/)
+docker-compose up --build --detach solr;
+
+# Apache: eZ Platform Install (needs Solr)
+docker-compose exec --user www-data apache rm -rf web/var/*;
+docker-compose exec --user www-data apache composer ezplatform-install;
+
+# Logs Follow-up
+#docker-compose logs --follow;
