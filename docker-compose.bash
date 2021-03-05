a6ab2cb8c0abc0c5aea060211da0ccd35c25daab
Adrien DUPUIS <adrien.dupuis@ez.no>
2021-03-05 19:19:23 +0100

    docker-compose.bash: Add options to choose containers

--- docker-compose.bash
+++ docker-compose.bash
@@ -8,6 +8,14 @@
 # Git: Untracked Files Removal
 #git clean -df; # Help to switch between eZ Platform v2 and v3
 
+# In-Place sed Command
+function sedi() {
+  ## Regular
+  #sed -i -e $*;
+  ## MacOS
+  sed -i '' -e "$1" $2;
+}
+
 # Apache: Composer Authentication
 if [ ! -f auth.json ]; then
   echo -n "eZ Platform Enterprise Edition Installation Key: "; read INSTALLATION_KEY;
@@ -22,16 +30,154 @@ composer install --no-interaction --no-scripts;
 # Solr: Copy config to build folder
 cp -r ./vendor/ezsystems/ezplatform-solr-search-engine/lib/Resources/config/solr ./docker/solr/conf;
 
-# Symfony: Cache and Logs Removal
-rm -rf var/cache/dev/ var/log/*.log var/encore/*config*.js;
-mkdir -p var/cache/dev; touch var/log/dev.log;
+# Symfony: Logs Removal
+rm -rf var/log/*.log;
+touch var/log/dev.log;
+
+# Symfony: Cache Removal
+rm -rf var/cache/dev/;
+mkdir -p var/cache/dev;
 
 # Symfony: Remove bin/ symlinks
-find bin/ -type l -exec unlink {} \; ; #
-rm -f ; # Remove Webpack Encore generated config files
+find bin/ -type l -exec unlink {} \; ; # Remove bin/ symlinks
+
+# Encore: Remove generated config files
+rm -f var/encore/*config*.js;
+
+# Docker: Parse Containers Options
+
+## Default Options Values
+cache='redis';
+search='solr';
+session='redis';
+dynamic_session=0;
+
+args=`getopt --options=hc:s:u:d $*`;
+set -- $args
+for i
+do
+  case "$i"
+  in
+    -h)
+      echo "Usage:";
+      echo "$0 [-c <filesystem|redis|memcached>] [-s <legacy|solr|elasticsearch|fallback>] [-u <filesystem|redis|memcached>] [-d]";
+      echo "-c: Cache Pool (default: redis)";
+      echo "-s: Search Engine (default: solr)";
+      echo "-s: User Sessions (default: redis); Not yet implemented";
+      echo "-d: Dynamic Session Handler (default: false); Not yet implemented";
+      ;;
+    -c)
+      cache="$2";
+      shift; shift;
+      ;;
+    -s)
+      search="$2";
+      shift; shift;
+      ;;
+    -u)
+      echo "Session Handler: Not yet implemented";
+      #session="$2";
+      shift; shift;
+      ;;
+    -d)
+      echo "Dynamic Session Handler: Not yet implemented";
+      #dynamic_session=1;
+      shift;
+      ;;
+  esac
+done
+
+case "$cache"
+in
+  'filesystem')
+    CACHE_POOL=cache.tagaware.filesystem;
+    CACHE_DSN=localhost;
+    cache_container='';
+    ;;
+  'redis')
+    CACHE_POOL=cache.redis;
+    CACHE_DSN=redis;
+    cache_container='redis';
+    ;;
+  'memcached')
+    CACHE_POOL=cache.memcached;
+    CACHE_DSN=memcached;
+    cache_container='memcached';
+    ;;
+esac
+
+case "$search"
+in
+  'legacy')
+    SEARCH_ENGINE=legacy;
+    search_container='';
+    ;;
+  'solr')
+    SEARCH_ENGINE=solr;
+    search_container='solr';
+    ;;
+  'elasticsearch')
+    SEARCH_ENGINE=elasticsearch;
+    search_container='elasticsearch';
+    ;;
+  'fallback')
+    SEARCH_ENGINE=fallback;
+    search_container='solr elasticsearch';
+    ;;
+esac
+
+case "$session"
+in
+  'filesystem')
+    SESSION_HANDLER_ID=session.handler.native_file
+    SESSION_SAVE_PATH=%kernel.project_dir%/var/sessions/%kernel.environment%
+    session_container='';
+    ;;
+  'redis')
+    SESSION_HANDLER_ID=ezplatform.core.session.handler.native_redis
+    SESSION_SAVE_PATH=tcp://redis:6379
+    session_container='redis';
+    ;;
+  'memcached')
+    SESSION_HANDLER_ID=ad.session.handler.native_memcached
+    SESSION_SAVE_PATH=memcached:11211
+    session_container='memcached';
+    ;;
+esac
+
+if [[ ! -f .env.local ]]; then
+  cp -v .env.pattern .env.local;
+fi
+
+# Docker/eZ: Set Environment
+
+sedi "s/CACHE_POOL=.*/CACHE_POOL=$CACHE_POOL/" .env.local;
+sedi "s/CACHE_DSN=.*/CACHE_DSN=$CACHE_DSN/" .env.local;
+
+sedi "s/SEARCH_ENGINE=.*/SEARCH_ENGINE=$SEARCH_ENGINE/" .env.local;
+
+if [[ 0 == $dynamic_session ]]; then
+  echo "Session Handler set in PHP";
+  sedi "s/^SESSION_HANDLER_ID=.*/#SESSION_HANDLER_ID=/" .env;
+  sedi "s/^SESSION_HANDLER_ID=.*/#SESSION_HANDLER_ID=/" .env.local;
+  sedi "s/ezplatform.session.handler_id: .*/ezplatform.session.handler_id: ~/" config/packages/ezplatform.yaml;
+  # TODO: PHP-FPM
+elif [[ 1 == $dynamic_session ]]; then
+  echo "Session Handler set dynamically";
+  sedi "s/^#SESSION_HANDLER_ID=/SESSION_HANDLER_ID=/" .env.local;
+  sedi "s/^SESSION_HANDLER_ID=.*/SESSION_HANDLER_ID=$SESSION_HANDLER_ID/" .env.local;
+  sedi "s/^SESSION_SAVE_PATH=.*/SESSION_SAVE_PATH=$SESSION_SAVE_PATH/" .env.local;
+  sedi "s/ezplatform.session.save_path: .*/ezplatform.session.save_path: '$SESSION_SAVE_PATH'/" config/packages/ezplatform.yaml;
+fi
+
+# Docker: docker-compose settings
+sedi "s/^COMPOSE_FILE=/#COMPOSE_FILE=/" .env;
+sedi "s/^COMPOSE_PROJECT_NAME=/#COMPOSE_FILE=/" .env;
 
 # Docker: Containers Cluster Build
-docker-compose up --build --detach;
+container_list="varnish apache mariadb $cache_container $search_container $session_container";
+container_list=`echo "$container_list" | awk '{for (i=1;i<=NF;i++) if (!a[$i]++) printf("%s%s",$i,FS)}{printf("\n")}'`;
+docker-compose up --build --detach $container_list;
 
 # Solr: Clean-up build folder
 rm -rf ./docker/solr/conf;
@@ -51,7 +197,8 @@ done;
 echo "MariaDB version: $MARIADB_VERSION";
 
 # Apache: Doctrine Configuration
-sed -i '' -e "s/DATABASE_VERSION=mariadb-.*/DATABASE_VERSION=mariadb-$MARIADB_VERSION/" .env;
+sedi "s/DATABASE_VERSION=.*/DATABASE_VERSION=mariadb-$MARIADB_VERSION/" .env;
+sedi "s/DATABASE_VERSION=.*/DATABASE_VERSION=mariadb-$MARIADB_VERSION/" .env.local;
 
 # Apache: Composer Scripts' Timeout
 docker-compose exec --user www-data apache composer config --global process-timeout 0;
