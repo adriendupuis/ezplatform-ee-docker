9819102c17c14cb446e103c6a373582ce48342ee
Adrien DUPUIS <adrien.dupuis@ez.no>
2021-09-22 17:07:19 +0200

    docker-compose.bash: composer update --lock --no-scripts;

--- docker-compose.bash
+++ docker-compose.bash
@@ -127,7 +127,7 @@ fi;
 
 # Symfony/eZ/Composer: Install dependencies
 composer config platform.php 7.3;
-composer update --lock;
+composer update --lock --no-scripts;
 composer install --no-interaction --no-scripts;
 
 # Solr: Copy config to build folder
